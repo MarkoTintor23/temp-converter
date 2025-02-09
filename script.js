c696800db2999eba389eca855d1fdcaac9cef4bd
Marko Tintor <marko.tintor.18@singimail.rs>
2025-02-09 13:22:15 +0100

    added classes

--- script.js
+++ script.js
@@ -1,69 +1,62 @@
-const celsius = document.querySelector("#celsius");
-const fahren = document.querySelector("#fahrenheit");
-const kelvin = document.querySelector("#kelvin");
-
-const celsiusToFahren = function (cel) {
-  return (cel * 9) / 5 + 32;
-};
-
-const celsiusToKelvin = function (cel) {
-  return cel + 273.15;
-};
-const farenToCelsius = function (far) {
-  return ((far - 32) * 5) / 9;
-};
-const fahrenToKelvin = function (far) {
-  return ((far - 32) * 5) / 9 + 273.15;
-};
-const kelvinToCelsius = function (kel) {
-  return kel - 273.15;
-};
-const kelvinToFahren = function (kel) {
-  return ((kel - 273.15) * 5) / 9 + 32;
-};
-
-celsius.addEventListener("input", function () {
-  if (celsius.value === "") {
-    fahren.value = "";
-    kelvin.value = "";
-    return;
-  }
-  const celsiusValue = parseFloat(celsius.value);
-  if (!isNaN(celsius.value)) {
-    fahren.value = celsiusToFahren(celsiusValue).toFixed(2);
-    kelvin.value = celsiusToKelvin(celsiusValue).toFixed(2);
-  } else {
-    fahren.value = "";
-    kelvin.value = "";
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  new TempConverter(); // Inicijalizacija klase nakon što se učita DOM
 });
-fahren.addEventListener("input", function () {
-  if (fahren.value === "") {
-    celsius.value = "";
-    kelvin.value = "";
-    return;
+class TempConverter {
+  constructor() {
+    this.celsius = document.querySelector("#celsius");
+    this.fahren = document.querySelector("#fahrenheit");
+    this.kelvin = document.querySelector("#kelvin");
+
+    this.addEventListeners();
   }
-  const fahrenValue = parseFloat(fahren.value);
-  if (!isNaN(fahren.value)) {
-    celsius.value = farenToCelsius(fahrenValue).toFixed(2);
-    kelvin.value = fahrenToKelvin(fahrenValue).toFixed(2);
-  } else {
-    celsius.value = "";
-    kelvin.value = "";
+
+  addEventListeners() {
+    this.celsius.addEventListener("input", (e) => this.handleInput(e));
+    this.fahren.addEventListener("input", (e) => this.handleInput(e));
+    this.kelvin.addEventListener("input", (e) => this.handleInput(e));
   }
-});
-kelvin.addEventListener("input", function () {
-  if (kelvin.value === "") {
-    celsius.value = "";
-    fahren.value = "";
-    return;
+
+  handleInput(e) {
+    const value = parseFloat(e.target.value);
+
+    if (isNaN(value)) {
+      this.clearFields();
+      return;
+    }
+
+    if (e.target === this.celsius) {
+      this.fahren.value = this.celsiusToFahren(value).toFixed(2);
+      this.kelvin.value = this.celsiusToKelvin(value).toFixed(2);
+    } else if (e.target === this.fahren) {
+      this.celsius.value = this.fahrenToCelsius(value).toFixed(2);
+      this.kelvin.value = this.fahrenToKelvin(value).toFixed(2);
+    } else if (e.target === this.kelvin) {
+      this.celsius.value = this.kelvinToCelsius(value).toFixed(2);
+      this.fahren.value = this.kelvinToFahren(value).toFixed(2);
+    }
   }
-  const kelvinValue = parseFloat(kelvin.value);
-  if (!isNaN(kelvin.value)) {
-    celsius.value = kelvinToCelsius(kelvinValue).toFixed(2);
-    fahren.value = kelvinToFahren(kelvinValue).toFixed(2);
-  } else {
-    celsius.value = "";
-    fehren.value = "";
+  celsiusToFahren(cel) {
+    return (cel * 9) / 5 + 32;
   }
-});
+
+  celsiusToKelvin(cel) {
+    return cel + 273.15;
+  }
+  fahrenToCelsius(far) {
+    return ((far - 32) * 5) / 9;
+  }
+  fahrenToKelvin(far) {
+    return ((far - 32) * 5) / 9 + 273.15;
+  }
+  kelvinToCelsius(kel) {
+    return kel - 273.15;
+  }
+  kelvinToFahren(kel) {
+    return ((kel - 273.15) * 5) / 9 + 32;
+  }
+  clearFields() {
+    this.celsius.value = "";
+    this.fahren.value = "";
+    this.kelvin.value = "";
+  }
+}
